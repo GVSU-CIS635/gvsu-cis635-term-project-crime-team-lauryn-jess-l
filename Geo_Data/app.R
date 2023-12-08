3461d7278179601628a8ec3ef52ea8ca15f75b57
Lauryn Davis <ldavis9997@yahoo.com>
2023-12-07 21:00:26 -0500

    wip: Moving the location of file

--- Geo_Data/app.R
+++ Geo_Data/app.R
@@ -19,6 +19,7 @@ library(leafgl)
 #Loading in Shape Files:
 janjuly2016 <- read_sf("Geo_Data/2016/NIJ2016_JAN01_JUL31.shp")
 aug2016<- read_sf("Geo_Data/2016/NIJ2016_AUG01_AUG31.shp")
+aug2016<- na.omit(aug2016)
 sept2016<- read_sf("Geo_Data/2016/NIJ2016_SEP01_SEP30.shp")
 oct2016<- read_sf("Geo_Data/2016/NIJ2016_OCT01_OCT31.shp")
 nov2016<- read_sf("Geo_Data/2016/NIJ2016_NOV01_NOV30.shp")
@@ -37,9 +38,12 @@ marchmay2017<- read_sf("Geo_Data/2017/NIJ2017_MAR01_MAY31.shp")
 
 #Full years: 2012 only has data from march - dec. 
 full_2012<-  read_sf("Geo_Data/2012/NIJ2012_MAR01_DEC31.shp")
+
 full_2013<- read_sf("Geo_Data/2013/NIJ2013_JAN01_DEC31.shp")
 full_2014<- read_sf("Geo_Data/2014/NIJ2014_JAN01_DEC31.shp")
+full_2014<- na.omit(full_2014)
 full_2015<- read_sf("Geo_Data/2015/NIJ2015_JAN01_DEC31.shp")
+full_2015<- na.omit(full_2015)
 
 # file merges:
 
@@ -52,19 +56,27 @@ full2017<- rbind(jan2017,feb1to142017,feb15to212017, feb22to26, feb27, feb28, ma
 #We cannot merge all years together until dbfs have been uploaded. 
 all_data <- rbind(full_2012, full_2013, full_2014, full_2015, full2016, full2017)
 
+#Final CRS Transformations: 
+finalfull2012<- st_transform(full_2012, crs = 4326)
+finalfull2013 <- st_transform(full_2013, crs=4326)
+finalfull2014<- st_transform(full_2014, crs = 4326)
+finalfull2015<- st_transform(full_2015, crs = 4326)
+finalfull2016<- st_transform(full2016, crs = 4326)
+finalfull2017<- st_transform(full2017, crs = 4326)
 
 ui<- fluidPage(
   navbarPage(
-    theme = shinytheme("united"),
+    theme = shinytheme("superhero"),
     "Portland Crime Analysis",
     tabPanel("Interactive Map",
-             actionButton("revert", "Reset Back to Full Portland View"),
-             sidebarPanel(width = 5, 
-
-                          selectInput("Year", 
-                                      label = "Choose a Year to Display",
-                                      choices = c("2012", "2013", "2014", "2015", "2016", "2017", "2018"),
-                                      selected = "2017")),
+             fluidRow(width = 2, 
+                          actionButton("revert", "Reset Back to Full Portland View", style='padding:2px; font-size:100%', class= 'btn btn-danger'),
+                          actionButton("2012", "2012", style='padding:2px; font-size:100%',  class = "btn-warning"),
+                          actionButton("2013", "2013", style='padding:2px; font-size:100%',  class = "btn-warning"),
+                          actionButton("2014", "2014", style='padding:2px; font-size:100%',  class = "btn-warning"),
+                          actionButton("2015", "2015", style='padding:2px; font-size:100%',  class = "btn-warning"),
+                          actionButton("2016", "2016", style='padding:2px; font-size:100%',  class = "btn-warning"),
+                          actionButton("2017", "2017", style='padding:2px; font-size:100%',  class = "btn-warning")),
              div(
                id = "map_container",
                leafletOutput(height = "700px", "map")
@@ -75,39 +87,136 @@ ui<- fluidPage(
   ))
 
 
-my_centers <<- list()
-
-finalfull2013 <- st_transform(full_2013, crs=4326)
-
-
 server<- function(input, output, session) {
-
+  
   output$map <-  renderLeaflet({
-    states_map$zoom <- sample(5:8, size = length(states_map$name), replace = T)
-    
-    leaflet(data = finalfull2013, options = leafletOptions(minZoom = 11, preferCanvas = TRUE, zoomToLimits="first"))%>%
-      addProviderTiles(provider = "CartoDB.Positron", options = providerTileOptions(
-        updateWhenZooming = FALSE,      # map won't update tiles until zoom is done
-        updateWhenIdle = FALSE          # map won't load new tiles when panning
-      ))%>%
-      addGlPoints(data = finalfull2013[finalfull2013$CALL_GROUP == 'DISORDER',], group = "Disorder",  fillColor = 'lightyellow',fillOpacity = 1, radius = 6, popup = ~finalfull2013$CALL_GROUP)%>%
-      addGlPoints(data = finalfull2013[finalfull2013$CALL_GROUP == 'NON CRIMINAL/ADMIN',],  fillColor = 'salmon',fillOpacity = 1, radius = 6, popup = ~finalfull2013$CALL_GROUP, group = 'Non Criminal/Admin')%>%
-      addGlPoints(data = finalfull2013[finalfull2013$CALL_GROUP == 'TRAFFIC',], fillColor = 'lightpink', popup = ~finalfull2013$CALL_GROUP, group = 'Traffic', fillOpacity = 5, radius = 6)%>%
-      addGlPoints(data = finalfull2013[finalfull2013$CALL_GROUP == 'SUSPICIOUS',],  fillColor = 'lightgrey', popup = ~finalfull2013$CALL_GROUP, group = 'Suspicious', fillOpacity = 5, radius = 6)%>%
-      addGlPoints(data = finalfull2013[finalfull2013$CALL_GROUP == 'PROPERTY CRIME',],fillColor = 'orange', popup = ~finalfull2013$CALL_GROUP, group = 'Property Crime', fillOpacity = 5, radius = 6)%>%
-      addGlPoints(data = finalfull2013[finalfull2013$CALL_GROUP == 'PERSON CRIME',],  fillColor = 'mediumaquamarine', popup = ~finalfull2013$CALL_GROUP, group = 'Person Crime', fillOpacity = 5, radius = 6)%>%
+    leaflet(options = leafletOptions(minZoom = 11, preferCanvas = TRUE, zoomToLimits="first"))%>%
+      addProviderTiles(provider = "CartoDB.Positron"       # map won't load new tiles when panning
+      )%>%
+      setView(lng = -122.676483, 
+              lat = 45.523064, 
+              zoom = 11)%>%
+      setMaxBounds(-122.75, 45.45, -122.5, 45.6)%>%
+      addLegend(colors = c('lightblue', 'salmon', 'lightpink', 'lightgrey', 'orange', 'mediumaquamarine'),labels = c("Disorder", "Non Criminal/Admin", "Traffic", "Suspicious", "Property Crime", "Person Crime" ), opacity = 1)%>%
       addLayersControl(overlayGroups = c("Disorder", "Non Criminal/Admin", "Traffic", "Suspicious", "Property Crime", "Person Crime" ),
                        options = layersControlOptions(collapsed =FALSE),
-                       position = 'bottomleft')%>%
-      addLegend(colors = c('lightyellow', 'salmon', 'lightpink', 'lightgrey', 'orange', 'mediumaquamarine'),labels = c("Disorder", "Non Criminal/Admin", "Traffic", "Suspicious", "Property Crime", "Person Crime" ), opacity = 1)%>%
+                       position = 'bottomleft')
+  })
+
+  observeEvent(input$"2012", {
+    
+    if(is.null(input$"2012"))
+      return()
+    leafletProxy('map')%>%
+      addGlPoints(data = finalfull2012[finalfull2012$CALL_GROUP == 'DISORDER',], group = "Disorder",  fillColor = 'lightblue',fillOpacity = 1, radius = 6, popup = "Disorder")%>%
+      addGlPoints(data = finalfull2012[finalfull2012$CALL_GROUP == 'NON CRIMINAL/ADMIN',],  fillColor = 'salmon',fillOpacity = 1, radius = 6, popup = 'Non Criminal/Admin', group = 'Non Criminal/Admin')%>%
+      addGlPoints(data = finalfull2012[finalfull2012$CALL_GROUP == 'TRAFFIC',], fillColor = 'lightpink', popup = 'Traffic', group = 'Traffic', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2012[finalfull2012$CALL_GROUP == 'SUSPICIOUS',],  fillColor = 'lightgrey', popup = 'Suspicious', group = 'Suspicious', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2012[finalfull2012$CALL_GROUP == 'PROPERTY CRIME',],fillColor = 'orange', popup = 'Property Crime', group = 'Property Crime', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2012[finalfull2012$CALL_GROUP == 'PERSON CRIME',],  fillColor = 'mediumaquamarine', popup = 'Person Crime', group = 'Person Crime', fillOpacity = 5, radius = 6)%>%
       setView(lng = -122.676483, 
               lat = 45.523064, 
               zoom = 11)%>%
       setMaxBounds(-122.75, 45.45, -122.5, 45.6)
+    
   })
   
-  observeEvent(input$revert, {
+  observeEvent(input$"2013", {
     
+    if(is.null(input$"2013"))
+      return()
+    leafletProxy('map')%>%
+      addGlPoints(data = finalfull2013[finalfull2013$CALL_GROUP == 'DISORDER',], group = "Disorder",  fillColor = 'lightblue',fillOpacity = 1, radius = 6, popup = "Disorder")%>%
+      addGlPoints(data = finalfull2013[finalfull2013$CALL_GROUP == 'NON CRIMINAL/ADMIN',],  fillColor = 'salmon',fillOpacity = 1, radius = 6, popup = 'Non Criminal/Admin', group = 'Non Criminal/Admin')%>%
+      addGlPoints(data = finalfull2013[finalfull2013$CALL_GROUP == 'TRAFFIC',], fillColor = 'lightpink', popup = 'Traffic', group = 'Traffic', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2013[finalfull2013$CALL_GROUP == 'SUSPICIOUS',],  fillColor = 'lightgrey', popup = 'Suspicious', group = 'Suspicious', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2013[finalfull2013$CALL_GROUP == 'PROPERTY CRIME',],fillColor = 'orange', popup = 'Property Crime', group = 'Property Crime', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2013[finalfull2013$CALL_GROUP == 'PERSON CRIME',],  fillColor = 'mediumaquamarine', popup = 'Person Crime', group = 'Person Crime', fillOpacity = 5, radius = 6)%>%
+
+      setView(lng = -122.676483, 
+              lat = 45.523064, 
+              zoom = 11)%>%
+      setMaxBounds(-122.75, 45.45, -122.5, 45.6)
+    
+  })
+  observeEvent(input$"2014", {
+    
+    if(is.null(input$"2014"))
+      return()
+    leafletProxy('map')%>%
+      addGlPoints(data = finalfull2014[finalfull2014$CALL_GROUP == 'DISORDER',], group = "Disorder",  fillColor = 'lightblue',fillOpacity = 1, radius = 6, popup = "Disorder")%>%
+      addGlPoints(data = finalfull2014[finalfull2014$CALL_GROUP == 'NON CRIMINAL/ADMIN',],  fillColor = 'salmon',fillOpacity = 1, radius = 6, popup = 'Non Criminal/Admin', group = 'Non Criminal/Admin')%>%
+      addGlPoints(data = finalfull2014[finalfull2014$CALL_GROUP == 'TRAFFIC',], fillColor = 'lightpink', popup = 'Traffic', group = 'Traffic', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2014[finalfull2014$CALL_GROUP == 'SUSPICIOUS',],  fillColor = 'lightgrey', popup = 'Suspicious', group = 'Suspicious', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2014[finalfull2014$CALL_GROUP == 'PROPERTY CRIME',],fillColor = 'orange', popup = 'Property Crime', group = 'Property Crime', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2014[finalfull2014$CALL_GROUP == 'PERSON CRIME',],  fillColor = 'mediumaquamarine', popup = 'Person Crime', group = 'Person Crime', fillOpacity = 5, radius = 6)%>%
+
+      setView(lng = -122.676483, 
+              lat = 45.523064, 
+              zoom = 11)%>%
+      setMaxBounds(-122.75, 45.45, -122.5, 45.6)
+    
+  })
+  
+  observeEvent(input$"2015", {
+    
+    if(is.null(input$"2015"))
+      return()
+    leafletProxy('map')%>%
+      addGlPoints(data = finalfull2015[finalfull2015$CALL_GROUP == 'DISORDER',], group = "Disorder",  fillColor = 'lightblue',fillOpacity = 1, radius = 6, popup = "Disorder")%>%
+      addGlPoints(data = finalfull2015[finalfull2015$CALL_GROUP == 'NON CRIMINAL/ADMIN',],  fillColor = 'salmon',fillOpacity = 1, radius = 6, popup = 'Non Criminal/Admin', group = 'Non Criminal/Admin')%>%
+      addGlPoints(data = finalfull2015[finalfull2015$CALL_GROUP == 'TRAFFIC',], fillColor = 'lightpink', popup = 'Traffic', group = 'Traffic', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2015[finalfull2015$CALL_GROUP == 'SUSPICIOUS',],  fillColor = 'lightgrey', popup = 'Suspicious', group = 'Suspicious', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2015[finalfull2015$CALL_GROUP == 'PROPERTY CRIME',],fillColor = 'orange', popup = 'Property Crime', group = 'Property Crime', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2015[finalfull2015$CALL_GROUP == 'PERSON CRIME',],  fillColor = 'mediumaquamarine', popup = 'Person Crime', group = 'Person Crime', fillOpacity = 5, radius = 6)%>%
+
+      setView(lng = -122.676483, 
+              lat = 45.523064, 
+              zoom = 11)%>%
+      setMaxBounds(-122.75, 45.45, -122.5, 45.6)
+    
+  })
+  
+  observeEvent(input$"2016", {
+    
+    if(is.null(input$"2016"))
+      return()
+    leafletProxy('map')%>%
+      addGlPoints(data = finalfull2016[finalfull2016$CALL_GROUP == 'DISORDER',], group = "Disorder",  fillColor = 'lightblue',fillOpacity = 1, radius = 6, popup = "Disorder")%>%
+      addGlPoints(data = finalfull2016[finalfull2016$CALL_GROUP == 'NON CRIMINAL/ADMIN',],  fillColor = 'salmon',fillOpacity = 1, radius = 6, popup = 'Non Criminal/Admin', group = 'Non Criminal/Admin')%>%
+      addGlPoints(data = finalfull2016[finalfull2016$CALL_GROUP == 'TRAFFIC',], fillColor = 'lightpink', popup = 'Traffic', group = 'Traffic', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2016[finalfull2016$CALL_GROUP == 'SUSPICIOUS',],  fillColor = 'lightgrey', popup = 'Suspicious', group = 'Suspicious', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2016[finalfull2016$CALL_GROUP == 'PROPERTY CRIME',],fillColor = 'orange', popup = 'Property Crime', group = 'Property Crime', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2016[finalfull2016$CALL_GROUP == 'PERSON CRIME',],  fillColor = 'mediumaquamarine', popup = 'Person Crime', group = 'Person Crime', fillOpacity = 5, radius = 6)%>%
+
+      setView(lng = -122.676483, 
+              lat = 45.523064, 
+              zoom = 11)%>%
+      setMaxBounds(-122.75, 45.45, -122.5, 45.6)
+    
+  })
+  
+  observeEvent(input$"2017", {
+    
+    if(is.null(input$"2017"))
+      return()
+    leafletProxy('map')%>%
+      addGlPoints(data = finalfull2017[finalfull2017$CALL_GROUP == 'DISORDER',], group = "Disorder",  fillColor = 'lightblue',fillOpacity = 1, radius = 6, popup = "Disorder")%>%
+      addGlPoints(data = finalfull2017[finalfull2017$CALL_GROUP == 'NON CRIMINAL/ADMIN',],  fillColor = 'salmon',fillOpacity = 1, radius = 6, popup = 'Non Criminal/Admin', group = 'Non Criminal/Admin')%>%
+      addGlPoints(data = finalfull2017[finalfull2017$CALL_GROUP == 'TRAFFIC',], fillColor = 'lightpink', popup = 'Traffic', group = 'Traffic', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2017[finalfull2017$CALL_GROUP == 'SUSPICIOUS',],  fillColor = 'lightgrey', popup = 'Suspicious', group = 'Suspicious', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2017[finalfull2017$CALL_GROUP == 'PROPERTY CRIME',],fillColor = 'orange', popup = 'Property Crime', group = 'Property Crime', fillOpacity = 5, radius = 6)%>%
+      addGlPoints(data = finalfull2017[finalfull2017$CALL_GROUP == 'PERSON CRIME',],  fillColor = 'mediumaquamarine', popup = 'Person Crime', group = 'Person Crime', fillOpacity = 5, radius = 6)%>%
+
+      setView(lng = -122.676483, 
+              lat = 45.523064, 
+              zoom = 11)%>%
+      setMaxBounds(-122.75, 45.45, -122.5, 45.6)
+    
+  })
+
+  observeEvent(input$revert, {
+     
     if(is.null(input$revert))
       return()
     leafletProxy('map') %>% 
@@ -116,7 +225,6 @@ server<- function(input, output, session) {
   })  
   
 }
-
 
 
 shinyApp(ui, server)
